29e6d8d903f28c475ab9023743d0cff383a604be
rohitsingh100 <rohitgola1240@gmail.com>
2023-09-16 23:23:41 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// let book1 ="";
+// let book2 ="";
+// let book3 ="";
+// let book4 ="";
+
+let book = ["Math","pysices","computer science", "Bio"];
+
+// for(i=0; i < book.length; i++){
+//     console.log(`Element ${i} is ${book[i]} \n`);
+book.forEach(myfu);
+function myfu(value){
+    console.log(value);
+}
