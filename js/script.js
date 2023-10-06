870400ba1a08c2ec0f0bcc225480dacb527f5da1
rohitsingh100 <rohitgola1240@gmail.com>
2023-10-06 23:36:44 +0530

    Tem-Con

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-// let book1 ="";
-// let book2 ="";
-// let book3 ="";
-// let book4 ="";
+// Table
+// function Table(num){
+//     // let num=2;
+//     document.write("This is a Table...<br>");
+// for(i=1;i<=10;i++){
+//                  // 2      X      1      =       2
+//     document.write(num + " X " +  i  + " = " + num*i + "<br>");
+//     }
+// }
+// Table(2);
+// Table(3);
 
-let book = ["Math","pysices","computer science", "Bio"];
 
-// for(i=0; i < book.length; i++){
-//     console.log(`Element ${i} is ${book[i]} \n`);
-book.forEach(myfu);
-function myfu(value){
-    console.log(value);
+function Add(){
+    if(arguments.length == 0){
+        console.log("No Value pass... !");
+    }
+    else{
+        let = sum=0;
+        for(i=0; i<arguments.length; i++){
+            sum = sum + arguments[i];
+        }
+        console.log(sum);
+    }
 }
+Add(2,8);
